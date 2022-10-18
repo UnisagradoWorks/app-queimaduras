1e5094eb98ffaa6627f2ac06ddbbe46f662dba5e
Paulo Ferrarez <paulo.ferrarez.filho@gmail.com>
2022-10-18 08:49:18 -0300

    page histórias

--- app/src/main/java/br/com/unisagrado/appqueimaduras/MainActivity.java
+++ app/src/main/java/br/com/unisagrado/appqueimaduras/MainActivity.java
@@ -1,8 +1,11 @@
 package br.com.unisagrado.appqueimaduras;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView cardHistorias = findViewById(R.id.cardHistorias);
+        CardView cardQuiz = findViewById(R.id.cardQuiz);
+        CardView cardCreditos = findViewById(R.id.cardCreditos);
+
+        cardHistorias.setOnClickListener(this::abrirHistorias);
+
+    }
+
+    private void abrirHistorias(View view){
+        Intent intent = new Intent(this, HistoriasActivity.class);
+        startActivity(intent);
     }
 }
