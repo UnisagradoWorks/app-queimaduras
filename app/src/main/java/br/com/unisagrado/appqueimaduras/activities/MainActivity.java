3a36d52e6c2d4017be804a7129a904197f74bf85
jescacelestino <jessicacelestino@outlook.com.br>
2022-11-11 19:30:54 -0300

    :sparkles: tela de quiz.

--- app/src/main/java/br/com/unisagrado/appqueimaduras/activities/MainActivity.java
+++ app/src/main/java/br/com/unisagrado/appqueimaduras/activities/MainActivity.java
@@ -15,28 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        views();
+    }
 
+    private void views() {
         CardView cardHistorias = findViewById(R.id.cardHistorias);
         CardView cardQuiz = findViewById(R.id.cardQuiz);
         CardView cardCreditos = findViewById(R.id.cardCreditos);
 
-        cardHistorias.setOnClickListener(this::abrirHistorias);
-        cardQuiz.setOnClickListener(this::abrirQuiz);
-        cardCreditos.setOnClickListener(this::abrirCreditos);
+        cardHistorias.setOnClickListener(this::abrir);
+        cardQuiz.setOnClickListener(this::abrir);
+        cardCreditos.setOnClickListener(this::abrir);
     }
 
-    private void abrirHistorias(View view){
-        Intent intent = new Intent(this, HistoriasActivity.class);
-        startActivity(intent);
-    }
-
-    private void abrirCreditos(View view) {
-        Intent intent = new Intent(this, CreditosActivity.class);
-        startActivity(intent);
-    }
-
-    private void abrirQuiz(View view) {
-        Intent intent = new Intent(this, QuizActivity.class);
-        startActivity(intent);
+    private void abrir(View view) {
+        if(view.getId() == R.id.cardHistorias) {
+            Intent intent = new Intent(this, HistoriasActivity.class);
+            startActivity(intent);
+        } else if(view.getId() == R.id.cardQuiz) {
+            Intent intent = new Intent(this, SelecaoQuizActivity.class);
+            startActivity(intent);
+        } else if(view.getId() == R.id.cardCreditos) {
+            Intent intent = new Intent(this, CreditosActivity.class);
+            startActivity(intent);
+        }
     }
 }
---
+++ app/src/main/java/br/com/unisagrado/appqueimaduras/activities/SelecaoQuizActivity.java
@@ -0,0 +1,16 @@
+package br.com.unisagrado.appqueimaduras.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.unisagrado.appqueimaduras.R;
+
+public class SelecaoQuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecao_quiz);
+    }
+}
